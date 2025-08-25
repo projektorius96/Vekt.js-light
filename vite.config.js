e4b724f2115a2b1853f97852a1aa0837719b84ca
LG <34185919+projektorius96@users.noreply.github.com>
2025-08-25 21:59:34 +0300

    removing vite.config.js, see desc.
    As titled: since WebSocket issue for Vite.js was apparently fixed by the community, the HTTP polling workaround is redundant and the only config at the time of committing this, thus removing the config file;

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-    server: {
-        watch: {
-        usePolling: true, // Enables polling for file changes, this may fix WebSocket culprit, however the trade-off - more work for CPU !
-        }
-    }
-})
