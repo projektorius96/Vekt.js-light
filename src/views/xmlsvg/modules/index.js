51bb45babe9dc13f82c0491e88966b789b3a735f
LG <34185919+projektorius96@users.noreply.github.com>
2025-10-18 20:36:39 +0300

    code-revision: making the code modular than ever before...

--- src/views/xmlsvg/modules/index.js
+++ src/views/xmlsvg/modules/index.js
@@ -14,12 +14,13 @@ const
             );
         }
     })
-    ,
-    /**
-        * @alias
-    */
-    [CASE, ATTR, UI_EVENT] = Array(3).fill(ENUM);
     ;
+
+/**
+    * @alias
+*/
+const
+    [CASE, ATTR, UI_EVENT] = Array(3).fill(ENUM);
 
 export { ENUM }
 
@@ -73,19 +74,22 @@ export function setPoints(points = [], scalingFactor = 1) {
  *   @param {Number} opts.fontSize   Default: 16
  *   @param {String} opts.fontStyle  Default: 'italic'
  *   @param {String} opts.fill       Default: 'black'
+ *   @param {Number} opts.dx         Fine-tune baseline offset (e.g. 0.1em)
  *   @param {Number} opts.dy         Fine-tune baseline offset (e.g. 0.1em)
  *   @param {Number} opts.scale      Apply local scale transform (default 1)
  */
-export function drawLabel({svg, text, x, y, scale = 1, overrides = {}}) {
+export function drawLabel({svg, text, x, y, overrides = {}}) {
 
     const {
-        dominantBaseline = 'alphabetic',
+        dominantBaseline = 'middle',
         textAnchor = 'middle',
-        fontFamily = 'serif',
+        fontFamily = 'cursive',
         fontStyle = 'italic',
         fill = 'black',
         fontSize = 16,
+        dx = '0em',
         dy = '0em',
+        scale = 2
     } = overrides;
 
     // Create text element
@@ -97,10 +101,11 @@ export function drawLabel({svg, text, x, y, scale = 1, overrides = {}}) {
         t.setAttribute('font-size', fontSize);
         t.setAttribute('font-style', fontStyle);
         t.setAttribute('fill', fill);
+        t.setAttribute('dx', dx);
         t.setAttribute('dy', dy);
         t.textContent = text;
 
-    svg.appendChild(/* g */t);
-    return /* g */t;
+    svg.appendChild(t);
+    return t;
 
 }
